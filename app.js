0dd0dbcc463aeb14ff3c459f8de44a75387fb93d
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-03-15 22:17:30 +0700

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
+const cors            = require('cors')
 var app = express();
 require('dotenv').config()
 const mongoose = require('mongoose')
@@ -8,7 +9,7 @@ mongoose.connect('mongodb://agnynureza:12345@ds115219.mlab.com:15219/apatudb')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors())
 app.use('/', index);
 
 module.exports = app;
