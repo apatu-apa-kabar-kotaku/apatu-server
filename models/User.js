2757b0c0d596f91241c28825703bdf65b9b3d4ec
agnynureza <rezaagny@gmail.com>
2018-03-15 17:26:37 +0700

    setting gcs

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
-const mongoose   = require('mongoose')
-const Schema     = mongoose.Schema
+// const mongoose   = require('mongoose')
+// const Schema     = mongoose.Schema
 
-const userSchema = new Schema({
+// const userSchema = new Schema({
 
-})
+// })
 
-let User         = mongoose.model('User', UserSchema)
+// let User         = mongoose.model('User', UserSchema)
 
-module.exports   = User;
+// module.exports   = User;
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,13 @@
-const express        = require('express');
-const router         = express.Router();
-const PostController = require('../controllers/PostController');
+const express           = require('express');
+const router            = express.Router();
+const PostController    = require('../controllers/PostController');
+const {sendUploadToGCS} = require('../middleware/uploadGCS') 
 
-router.get('/', function(req, res, next) {
-  res.send('This is post resource');
+router.post('/upload', sendUploadToGCS ,function(req, res) {
+  res.status(201).json({
+    message : "upload success",
+    data : req.file.cloudStoragePublicUrl
+  })
 });
 
 module.exports = router;
