4a384fbb2e6862476f4d90aa92a44292373fd20f
agnynureza <rezaagny@gmail.com>
2018-03-16 11:38:43 +0700

    add search

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express           = require('express');
 const router            = express.Router();
-const {uploadFile,readFile,findByIdUserPost,deletePost,findByCategories, upvote, downvote}     = require('../controllers/PostController');
+const {uploadFile,readFile,findByIdUserPost,deletePost,findByCategories, upvote, downvote,searchTitle}     = require('../controllers/PostController');
 const {sendUploadToGCS} = require('../middleware/uploadGCS') 
 const multer = require('multer')
 
@@ -13,9 +13,10 @@ const upload = multer({
 
 router.get('/',readFile)
 router.get('/:user_id',findByIdUserPost)
-router.get('/:categories',findByCategories)
+router.get('/category/:category',findByCategories)
 router.post('/upload',upload.single('post'),sendUploadToGCS,uploadFile)
-router.delete('/:user_id/:id',deletePost) 
+router.delete('/:id_post',deletePost)
+router.post('/search',searchTitle) 
 router.post('/upvote', upvote)
 router.post('/downvote', downvote)
 
